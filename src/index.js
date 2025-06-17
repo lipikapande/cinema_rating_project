892e9b2f2a29fb780632d5a8b0ea35e3636eb597
lipika <lipikapande@gmail.com>
2025-06-17 19:14:22 +0530

    star rating reusable component

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
   </React.StrictMode>
 );
